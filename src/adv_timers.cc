cc26d0afc09730124a893956f592cb6cf182c854
Will-Irvin <willirvin03@gmail.com>
2023-05-26 17:32:25 -0500

    Finished lesson 23

---
+++ src/adv_timers.cc
@@ -0,0 +1,169 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <string>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+
+#include "LTexture.hh"
+#include "LTimer.hh"
+
+#define SCREEN_WIDTH (640)
+#define SCREEN_HEIGHT (480)
+
+bool init(SDL_Window**, SDL_Renderer**);
+bool loadMedia(LTexture*, SDL_Renderer*, TTF_Font**);
+void closeSDL(SDL_Window**, SDL_Renderer**, TTF_Font**, LTexture*, int);
+
+// Initialize SDL, Window, Renderer, Image, and TTF
+bool init(SDL_Window** window, SDL_Renderer** renderer) {
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		std::cout << "Init Error: " << SDL_GetError() << '\n';
+		return false;
+	}
+	if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
+		std::cout << "Warning: Linear texture filtering not enabled\n";
+	}
+	*window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED,
+														 SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, 
+														 SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (*window == NULL) {
+		std::cout << "Window creation error: " << SDL_GetError() << '\n';
+		return false;
+	}
+
+	// PRESENT_VSYNC flag has SDL uptate at the same time as the monitor during vertical refresh
+	// Default is 60 fps
+	*renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
+	if (*renderer == NULL) {
+		std::cout << "Renderer creation error: " << SDL_GetError() << '\n';
+		return false;
+	}
+
+	SDL_SetRenderDrawColor(*renderer, 0xff, 0xff, 0xff, 0xff);
+
+	int imgFlags = IMG_INIT_PNG;
+	if (!(IMG_Init(imgFlags) & imgFlags)) {
+		std::cout << "SDL_Image init error: " << IMG_GetError() << '\n';
+		return false;
+	}
+
+	if (TTF_Init() == -1) {
+		std::cout << "TTF Init error: " << TTF_GetError() << '\n';
+		return false;
+	}
+	return true;
+}
+
+bool loadMedia(LTexture* texture, SDL_Renderer* renderer, TTF_Font** font_ptr) {
+	*font_ptr = TTF_OpenFont("fonts/lazy.ttf", 28);
+	if (*font_ptr == NULL) {
+		std::cout << "Failed to load lazy font: " << TTF_GetError() << '\n';
+		return false;
+	}
+
+	SDL_Color textColor = {0, 0, 0};
+
+	texture[0] = LTexture();
+	texture[1] = LTexture();
+	texture[2] = LTexture();
+
+	// Note make as few of these calls as possible
+	if (!texture[0].loadFromRenderedText("Press s to start and stop the timer.",
+																		 textColor, renderer, *font_ptr))	{
+		return false;
+	}
+
+	if (!texture[1].loadFromRenderedText("Press p to pause and unpause the timer.",
+																			 textColor, renderer, *font_ptr)) {
+		return false;
+	}
+	return true;
+}
+
+void closeSDL(SDL_Window** window, SDL_Renderer** renderer, TTF_Font** font_ptr,
+							LTexture* textures, int numTextures) {
+	for (int i = 0; i < numTextures; i++) {
+		textures[i].free();
+	}
+
+	TTF_CloseFont(*font_ptr);
+	*font_ptr = NULL;
+
+	SDL_DestroyRenderer(*renderer);
+	*renderer = NULL;
+	SDL_DestroyWindow(*window);
+	*window = NULL;
+
+	SDL_Quit();
+	IMG_Quit();
+	TTF_Quit();
+}
+
+int main(int argc, char** argv) {
+	SDL_Window* window = NULL;
+	SDL_Renderer* renderer = NULL;
+	TTF_Font* font = NULL;
+
+	LTexture textures[3];
+
+	if (!init(&window, &renderer)) {
+		return -1;
+	}
+	if (!loadMedia(textures, renderer, &font)) {
+		return -1;
+	}
+
+	SDL_Event e;
+	bool quit = false;
+	SDL_Color text_color = {0, 0, 0, 0xff};
+
+	std::stringstream timeText; // Memory text stream
+	LTimer timer = LTimer();
+
+	while (!quit) {
+		while (SDL_PollEvent(&e) != 0) {
+			if (e.type == SDL_QUIT) {
+				quit = true;
+			} else if (e.type == SDL_KEYDOWN) {
+				if (e.key.keysym.sym == SDLK_s) {
+					if (timer.isStarted()) {
+						timer.stop();
+					} else {
+						timer.start();
+					}
+				} else if (e.key.keysym.sym == SDLK_p) {
+					if (timer.isPaused()) {
+						timer.unpause();
+					} else {
+						timer.pause();
+					}
+				}	
+			}
+		}
+
+		timeText.str(""); // Set time text as empty string
+		// Fill stream with next text
+		timeText << "Seconds since start time: " << timer.getTicks() / 1000;
+
+		textures[2].loadFromRenderedText(timeText.str().c_str(), text_color,
+																			 renderer, font);
+
+		SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
+		SDL_RenderClear(renderer);
+
+		// Render text centered with the prompt texture
+		textures[0].render(renderer, (SCREEN_WIDTH - textures[0].getWidth()) / 2, 0);
+		textures[1].render(renderer, (SCREEN_WIDTH - textures[1].getWidth()) / 2,
+											 textures[0].getHeight());
+		textures[2].render(renderer, (SCREEN_WIDTH - textures[0].getWidth()) / 2, 
+									 (SCREEN_HEIGHT - textures[2].getHeight()) / 2);
+
+		SDL_RenderPresent(renderer);
+	}
+
+	closeSDL(&window, &renderer, &font, textures, 3);
+	return 0;
+}
